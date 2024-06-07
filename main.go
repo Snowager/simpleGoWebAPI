57c47c3b97cef6a3ff03dbb2c5dc7ebc28d210be
Cole Robinson <59709492+Snowager@users.noreply.github.com>
2024-06-06 23:46:19 -0600

    update main

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-
-	"github.com/Snowager/simpleGoWebAPI/server"
 )
 
 func main() {
 	port := 4000
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", server.Home)
+	mux.HandleFunc("/", apiServer.Home)
 
 	//log server start
 	log.Printf("Starting server on %v", port)
--- server/server.go
+++ server/server.go
@@ -1,4 +1,4 @@
-package server
+package apiServer
 
 import (
 	"net/http"
