af148ea7e77e08de726af91db8d5ed5862e1575a
Cole Robinson <59709492+Snowager@users.noreply.github.com>
2024-06-06 23:44:30 -0600

    update main

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/Snowager/simpleGoWebAPI/webAPI
+module github.com/Snowager/simpleGoWebAPI
 
 go 1.22.2
+
+require github.com/Snowager/simpleGoWebAPI/server v0.0.0-20240607051553-4c584c86207f
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/Snowager/simpleGoWebAPI/server"
 )
 
 func main() {
 	port := 4000
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/", server.Home)
 
 	//log server start
 	log.Printf("Starting server on %v", port)
